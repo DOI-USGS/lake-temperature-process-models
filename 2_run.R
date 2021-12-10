2982bc24f3484711ed90414ea863f46494fdba69
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-12-10 17:46:32 -0600

    Add placeholders for model execution targets and script

---
+++ 2_run.R
@@ -0,0 +1,3 @@
+source('2_run/src/run_glm3.R')
+
+p2 <- list()
---
+++ 2_run/src/run_glm3.R
@@ -0,0 +1,5 @@
+# write function to take in meteo_xwalk (one row per model run)
+# set up model sim dir
+# modify and write nml, with correct meteo filename NOT that in nml_list
+# read in meteo data and add burn-in and burn-out
+# write meteo data to sim dir
