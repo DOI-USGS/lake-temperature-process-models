a03a38df0af31262044b305029c32ab86c9f3615
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-12-14 19:53:13 -0600

    revise run code to use retry and tryCatch, finetune approach to combine feathers

--- 2_run.R
+++ 2_run.R
@@ -1,15 +1,40 @@
 source('2_run/src/run_glm3.R')
 
 p2 <- list(
+  # Function will generate file
+  # but return a tibble that includes that filename and its hash
   tar_target(
-    p2_glm_uncalibrated,
+    p2_glm_uncalibrated_runs,
     run_glm3_model(
-      sim_dir = '2_run/out',
+      sim_dir = '2_run/tmp',
       nml_objs = p1_nml_objects,
-      meteo_xwalk = p1_meteo_xwalk),
-    packages = c('glmtools', 'GLM3r'),
-    pattern = map(p1_meteo_xwalk)
-  )
-  # next, pull output
-  # and combine output for each lake, for each gcm, into single feather file
+      meteo_xwalk = p1_meteo_xwalk,
+      export_fl_template = '2_run/tmp/%s_%s_%s.feather'),
+    packages = c('retry','glmtools', 'GLM3r'),
+    pattern = map(p1_meteo_xwalk)),
+  # Group model runs by lake id and gcm
+  tar_target(
+    p2_glm_uncalibrated_run_groups,
+    p2_glm_uncalibrated_runs %>% 
+      group_by(lake_id, gcm) %>% 
+      tar_group(),
+    iteration = "group"
+  ),
+  # Use grouped target to combine glm output into feather files
+  tar_target(
+    p2_glm_uncalibrated_output_feathers,{  
+      # set filename
+      outfile <- sprintf('2_run/out/%s_%s.feather',
+                         unique(p2_glm_uncalibrated_run_groups$lake_id),
+                         unique(p2_glm_uncalibrated_run_groups$gcm))
+      if(!dir.exists(dirname(outfile))) dir.create(dirname(outfile), recursive=TRUE)
+      
+      # combine into single feather file and write
+      purrr::map_df(p2_glm_uncalibrated_run_groups$export_fl, function(export_file) {
+        arrow::read_feather(export_file)
+      }) %>% arrow::write_feather(outfile)
+      
+      return(outfile)},
+    format = 'file',
+    pattern = map(p2_glm_uncalibrated_run_groups))
 )
--- 2_run/src/run_glm3.R
+++ 2_run/src/run_glm3.R
@@ -1,104 +1,150 @@
-#' @Title run glm3 simulation
+#' @title Munge meteo and add burn-in and burn-out
+#' @description function to read in the raw meteorological data
+#' form the file specified in the meteo_xwalk, filter it based
+#' on the specified time_period, and add burn-in and burn-out
+#' periods by mirroring the data. From Jordan's GLM projection code: 
+#' https://github.com/jread-usgs/lake-temperature-process-models/blob/master/3_run/src/run_glm_utils.R#L62-L76
+#' @param meteo_xwalk a lake-gcm-time_period crosswalk table with the 
+#'  meteo file name and hash for each model run
+#'  @param begin the begin date for the model time_period
+#'  @param end the end date for the model_time_period
+#'  @param burn_in length of burn-in period, in days
+#'  @param burn_out length of burn-out period, in days
+#'  @return a munged dataframe of meteorological data that includes
+#'  burn-in and burn-out time
+munge_meteo_w_burn_in_out <- function(meteo_xwalk, begin, end, burn_in, burn_out){
+  meteo_data <- arrow::read_feather(meteo_xwalk$meteo_fl) %>%
+    mutate(time = as.Date(time)) %>% filter(time >= as.Date(begin) & time <= as.Date(end)) %>%
+    mutate(Rain = case_when(Snow > 0 ~ 0, TRUE ~ Rain))
+  # create a burn-in. We're going to mirror the data of the first year:
+  burn_in_data <- meteo_data[2:burn_in, ] %>% arrange(desc(time)) %>%
+    mutate(time = seq(from = meteo_data[1,]$time-burn_in+1, by = 'day', length.out = length(2:burn_in)))
+  # bind with real data, prior to the begin date. We're not duplicating dates, hence the "2"
+  meteo_data <- bind_rows(burn_in_data, meteo_data)
+  # create a burn-out. Mirror the final data:
+  burn_out_data <- tail(meteo_data, burn_out) %>% head(-1L) %>% arrange(desc(time)) %>%
+    mutate(time = seq(from = tail(meteo_data,1)$time+1, by = 'day', length.out = length(2:burn_out)))
+  # bind with real data, at the end of the sequence
+  meteo_data <- bind_rows(meteo_data, burn_out_data)
+  return(meteo_data)
+}
+
+#' @title function to extract and write glm output following a model run
+#' @description Extract temperature and ice data from the model
+#' run and save to the export feather file. 
+#' From Jordan's GLM projection code: 
+#' https://github.com/jread-usgs/lake-temperature-process-models/blob/master/3_run/src/run_glm_utils.R#L89-L101
+#' @param sim_lake_dir the directory for the model simulation
+#' @param nml_obj the complete nml object for the model run
+#' @param export_fl the filepath for the exported feather file
+extract_glm_output <- function(sim_lake_dir, nml_obj, export_fl) {
+  out_dir <- glmtools::get_nml_value(nml_obj, arg_name = 'out_dir')
+  out_fn <- paste0(glmtools::get_nml_value(nml_obj, 'out_fn'), '.nc')
+  nc_filepath = file.path(sim_lake_dir, out_dir, out_fn)
+  lake_depth <- glmtools::get_nml_value(nml_obj, arg_name = 'lake_depth')
+  export_depths <- seq(0, lake_depth, by = 0.5)
+  temp_data <- glmtools::get_temp(nc_filepath, reference = 'surface', z_out = export_depths) %>%
+    mutate(date = as.Date(lubridate::floor_date(DateTime, 'days'))) %>% select(-DateTime)
+  glmtools::get_var(nc_filepath, var_name = 'hice') %>%
+    dplyr::mutate(ice = hice > 0, date = as.Date(lubridate::ceiling_date(DateTime, ' days'))) %>% dplyr::select(-hice, -DateTime) %>%
+    dplyr::left_join(temp_data, ., by = 'date') %>%
+    select(time = date, everything()) %>%
+    feather::write_feather(export_fl)
+}
+
+#' @title run glm3 simulation
 #' @description For each lake - gcm - time period combo, write the nml
 #'  file, the meteo data, and run the glm3 model
 #'  @param sim_dir base directory for simulations
 #'  @param nml_objs list of nml objects (one per lake id), named by lake id
 #'  @param meteo_xwalk a lake-gcm-time_period crosswalk table with the 
 #'  meteo file name and hash for each model run
-run_glm3_model <- function(sim_dir, nml_objs, meteo_xwalk) {
+run_glm3_model <- function(sim_dir, nml_objs, meteo_xwalk, export_fl_template) {
   # pull lake_id from meteo_xwalk
   lake_id <- meteo_xwalk$site_id
   time_period <- meteo_xwalk$time_period
   gcm <- meteo_xwalk$gcm
   
   # prepare to write inputs and results locally for quick I/O
-  sim_lake_dir <- file.path(sim_dir, lake_id, gcm, time_period)
+  sim_lake_dir <- file.path(sim_dir, sprintf('%s_%s_%s', lake_id, gcm, time_period))
   dir.create(sim_lake_dir, recursive=TRUE, showWarnings=FALSE)
-  # FOR NOW - commenting out code to delete directory after model execution
-  # since not yet extracting output of model into feather file
-  # on.exit(unlink(sim_lake_dir, recursive = TRUE))
+  # delete sim_lake_dir after model has run and we've extracted the results
+  on.exit(unlink(sim_lake_dir, recursive = TRUE))
   
-  # Define data start and stop dates
+  # Define time period begin and end dates
   times <- strsplit(time_period,'_')[[1]]
-  start <- sprintf('%s-01-01', times[1])
-  stop <- sprintf('%s-12-31', times[2])
+  time_period_begin <- sprintf('%s-01-01', times[1])
+  time_period_end <- sprintf('%s-12-31', times[2])
   
-  # Set up burn in and burn out
-  tmp_data <- arrow::read_feather(meteo_xwalk$meteo_fl) %>%
-    mutate(time = as.Date(time)) %>% filter(time >= as.Date(start) & time <= as.Date(stop)) %>%
-    mutate(Rain = case_when(Snow > 0 ~ 0, TRUE ~ Rain))
-  burn_in <- 300 # days
-  burn_out <- 190 # days
-  # create a burn-in. We're going to mirror the data of the first year:
-  burn_in_data <- tmp_data[2:burn_in, ] %>% arrange(desc(time)) %>%
-    mutate(time = seq(from = tmp_data[1,]$time-burn_in+1, by = 'day', length.out = length(2:burn_in)))
-  # bind with real data, prior to the start. We're not duplicating dates, hence the "2"
-  tmp_data <- bind_rows(burn_in_data, tmp_data)
-  # create a burn-out. Mirror the final data:
-  burn_out_data <- tail(tmp_data, burn_out) %>% head(-1L) %>% arrange(desc(time)) %>%
-    mutate(time = seq(from = tail(tmp_data,1)$time+1, by = 'day', length.out = length(2:burn_out)))
-  # bind with real data, at the end of the sequence
-  tmp_data <- bind_rows(tmp_data, burn_out_data)
-  # write to the simulation file
-  readr::write_csv(tmp_data, file.path(sim_lake_dir, 'meteo_fl.csv'))
+  # Read in meteo data, add burn in and burn out and save to sim_lake_dir
+  meteo_data <- munge_meteo_w_burn_in_out(meteo_xwalk, time_period_begin, time_period_end, 
+                                          burn_in = 300, burn_out = 190)
+  sim_meteo_filename <- 'meteo_fl.csv'
+  readr::write_csv(meteo_data, file.path(sim_lake_dir, sim_meteo_filename))
+  
+  # Define simulation start and stop dates using munged meteo data
+  sim_start <- format(meteo_data[1,]$time, "%Y-%m-%d")
+  sim_stop <- format(tail(meteo_data, 1L)$time, "%Y-%m-%d")
   
   # write nml file, specifying meteo file and start and stop dates:
   nml_obj <- nml_objs[[lake_id]]
-  # out_fn <- paste0(glmtools::get_nml_value(nml_obj, 'out_fn'), '.nc')
-  nml_obj <- set_nml(nml_obj, arg_list = list(nsave = 24, meteo_fl = 'meteo_fl.csv',
-                                              lake_name = lake_id,
-                                              stop = format(tail(tmp_data, 1L)$time, "%Y-%m-%d"), 
-                                              start = format(tmp_data[1,]$time, "%Y-%m-%d")))
+  nml_obj <- set_nml(nml_obj, arg_list = list(nsave = 24, 
+                                              meteo_fl = sim_meteo_filename,
+                                              sim_name = sprintf('%s_%s_%s', lake_id, gcm, time_period),
+                                              start = sim_start,
+                                              stop = sim_stop))
   nml_obj$light$Kw_file <- NULL # use static value instead of relying on a file
   glmtools::write_nml(nml_obj, file.path(sim_lake_dir, 'glm3.nml'))
   
-  # run once with hourly output:
-  # FOR NOW - running w/ Alison's code to help debug
-  # eventually, will just run with:
-  # GLM3r::run_glm(sim_lake_dir, verbose = FALSE)
-  glm_time <- system.time({
-    glm_code <- tryCatch(
-      {
-        try1_code <- GLM3r::run_glm(sim_lake_dir, verbose = FALSE)
-        if(try1_code != 0) stop()
-        try1_code
-      },
-      error = function(e) {
-        message('GLM failed; retrying with verbose = TRUE')
-        try2_code <- GLM3r::run_glm(sim_lake_dir, verbose = TRUE)
-        try2_code
-      })
-  })[['elapsed']]
-  message(sprintf('GLM ran in %0.0f:%02.0f (M:S)', floor(glm_time/60), glm_time %% 60))
-  if(glm_code != 0) {
-    warning(sprintf("GLM code for %s suggests a problem: %s", lake_id, glm_code))
-    return(tibble(
-      run_date = Sys.time(),
-      lake_id = lake_id,
-      gcm = gcm,
-      time_period = time_period,
-      glm_time_s = glm_time,
-      glm_success = FALSE,
-      glm_code = glm_code,
-      glm_hash = NA))
-  }
-
-  # FOR NOW
-  # return a 1-row tibble of information about this model run
-  return(tibble(
+  # Set up export tibble
+  export_fl <- sprintf(export_fl_template, lake_id, gcm, time_period)
+  export_tibble <- tibble(
     run_date = Sys.time(),
     lake_id = lake_id,
     gcm = gcm,
     time_period = time_period,
-    glm_time_s = glm_time,
-    glm_success = TRUE,
-    glm_code = glm_code,
-    glm_hash = dir(file.path(sim_lake_dir, 'out'), full.names = TRUE) %>%
-      purrr::map_chr(tools::md5sum) %>% # hash each of the output files in the sim directory
-      digest::digest('sha1') # combine the file hashes into a single object hash
-  ))
+    export_fl = export_fl)
   
-  # TODO - extract output of model run and save in feather file
-  # In separate step, output for each lake, for each gcm, will
-  # be combined into a single feather file (one w/ output for all 3 time periods)
+  # for each model run, try running the model up to 5 times
+  # if model run works, report how long that model took to run
+  # and extract output and save to feather export file
+  # return tibble with export file name and hash
+  # if model run fails after 5 attempts, return
+  # tibble indicating that it failed and giving the code
+  tryCatch(
+    {
+      retry::retry(
+        {glm_time <- system.time({glm_code <- GLM3r::run_glm(sim_lake_dir, verbose = FALSE)})[['elapsed']]},
+        until=function(val, cnd) glm_code==0,
+        max_tries = 5)
+      
+      # make sure glm did succeed
+      if(glm_code != 0) stop()
+      
+      # extract output
+      extract_glm_output(sim_lake_dir, nml_obj, export_fl)
+      
+      # Add glm run information to export tibble
+      export_tibble <- export_tibble %>%
+        mutate(export_hash = tools::md5sum(export_fl),
+               glm_time_s = glm_time,
+               glm_success = TRUE,
+               glm_code = glm_code)
+      return(export_tibble)
+    },
+    error = function(e) {
+      glm_time <- system.time({glm_code <- GLM3r::run_glm(sim_lake_dir, verbose = FALSE)})[['elapsed']]
+      
+      # Make sure glm did indeed fail
+      if(glm_code == 0) stop()
+      
+      # Add glm run information to export tibble
+      export_tibble <- export_tibble %>%
+        mutate(export_hash = tools::md5sum(export_fl),
+               glm_time_s = glm_time,
+               glm_success = FALSE,
+               glm_code = glm_code)
+      return(export_tibble)
+    }
+  )
 }
