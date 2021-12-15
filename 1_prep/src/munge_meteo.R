0211c344b54314625c64f5bfe21e02e11fe8e6b2
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-12-15 13:23:49 -0600

    Remove unused meteo_branch column in meteo_xwalk and rename hash column

--- 1_prep/src/munge_meteo.R
+++ 1_prep/src/munge_meteo.R
@@ -46,8 +46,7 @@ build_meteo_xwalk <- function(meteo_feathers, lake_cell_xwalk, gcm_names, gcm_da
   # Build tibble of meteo files, branches, hashes, gcm name, cell_no, and time_period
   meteo_branches <- tibble(
     meteo_fl = meteo_feathers,
-    meteo_branch = names(meteo_feathers),
-    meteo_data = map_chr(meteo_branch, ~ tar_meta(starts_with(.x))$data),
+    meteo_fl_hash = map_chr(names(meteo_feathers), ~ tar_meta(starts_with(.x))$data),
     gcm = stringr::str_extract(meteo_fl, gcm_name_list),
     time_period = stringr::str_extract(meteo_fl, gcm_date_list),
     # cell_no is the very last part of the file name and so this str_extract() is 
