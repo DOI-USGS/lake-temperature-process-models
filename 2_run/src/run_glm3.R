e6de6f77f0f24fa4729f4b80d338abc06991fd13
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-02-10 14:29:12 -0600

    add return information

--- 2_run/src/run_glm3.R
+++ 2_run/src/run_glm3.R
@@ -162,6 +162,7 @@ run_glm3_model <- function(sim_dir, nml_objs, model_config, burn_in, burn_out, e
         }
       )
       # If extraction error is NOT NA, trigger error
+      # and return the error export_tibble
       if (!is.na(extraction_error)) stop()
       
       # If output was extracted, delete simulation directory
