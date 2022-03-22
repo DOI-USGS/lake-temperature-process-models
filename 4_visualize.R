0061c078913249362149ed0ba4b1a1acbe6d962e
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-03-22 18:18:32 -0500

    Add draft diagnostic plots

--- 4_visualize.R
+++ 4_visualize.R
@@ -1,1 +1,39 @@
 source('4_visualize/src/plot_results.R')
+
+p4 <- list(
+  # 
+  tar_target(
+    p4_20yr_preds,
+    plot_20yr_preds(p2_glm_uncalibrated_run_groups, 
+                    outfile_template <- '4_visualize/out/Site_20yr_preds_ice_%s_%s.png'),
+    pattern = map(p2_glm_uncalibrated_run_groups),
+    format='file'),
+  
+  tar_target(
+    p4_temp_density_gcms,
+    plot_temp_density_gcms(p2_glm_uncalibrated_run_groups,
+                        outfile = '4_visualize/out/temperature_density_gcms.png'),
+    format='file'
+  ),
+  
+  tar_target(
+    p4_temp_density_months,
+    plot_temp_density_months(p2_glm_uncalibrated_run_groups,
+                             outfile = '4_visualize/out/temperature_density_months.png'),
+    format='file'
+  ),
+  
+  tar_target(
+    p4_temp_violin_gcms,
+    plot_temp_violin_gcms(p2_glm_uncalibrated_run_groups,
+                            outfile = '4_visualize/out/temperature_violin_gcms.png'),
+    format='file'
+  ),
+  
+  tar_target(
+    p4_temp_violin_months,
+    plot_temp_violin_months(p2_glm_uncalibrated_run_groups,
+                             outfile = '4_visualize/out/temperature_violin_months.png'),
+    format='file'
+  )
+)
--- 4_visualize/src/plot_results.R
+++ 4_visualize/src/plot_results.R
@@ -1,18 +1,228 @@
-library(targets)
-library(tidyverse)
-library(lubridate)
-library(ggplot2)
+# glm_preds <- run_groups %>%
+#   group_by(site_id, gcm) %>%
+#   group_modify(~ {
+#     site_meteo_files <- .x$raw_meteo_fl
+#     site_export_files <- .x$export_fl
+#     purrr::map2_df(site_meteo_files, site_export_files, function(raw_meteo_fl, export_file) {
+#       # Define time period begin and end dates from raw meteo_fl
+#       meteo_data <- arrow::read_feather(raw_meteo_fl, col_select = "time")
+#       begin <- min(meteo_data$time)
+#       end <- max(meteo_data$time)
+#       # read in data for that time period and truncate
+#       arrow::read_feather(export_file) %>%
+#         filter(time >= as.Date(begin) & time <= as.Date(end))
+#     }) %>%
+#       select(ice, everything())
+#   }) %>%
+#   mutate(date = as.Date(time), .before=1) %>%
+#   select(-time) %>%
+#   mutate("year"= year(date), "doy"= yday(date),
+#          "decade" = sprintf('%ss', year - (year %% 10)),
+#          "period" = sprintf('%s - %s', year - (year %% 20), (year - (year %% 20)) + 20),
+#          .after=date) %>%
+#   filter(!(year == 2000))
+# 
+# glm_preds_long <- munge_long(glm_preds)
 
-output_files <- list.files('2_run/out/Denali','*.feather')
-tar_load(p1_gcm_names)
-lake_ids <- c('nhdhr_91597227','nhdhr_72863673','nhdhr_59724783','nhdhr_47719227','nhdhr_45361793',
-              'nhdhr_166867643','nhdhr_120020627','nhdhr_120018623','nhdhr_114542327','nhdhr_114539927')
-gcm_names <- p1_gcm_names 
+plot_temp_density_gcms <- function(run_groups, outfile) {
+  glm_preds <- run_groups %>%
+    group_by(site_id, gcm) %>%
+    group_modify(~ {
+      site_meteo_files <- .x$raw_meteo_fl
+      site_export_files <- .x$export_fl
+      site_preds <- get_site_preds(site_meteo_files, site_export_files)
+      
+      site_preds_long <- munge_long(site_preds)
+      
+      plot_depths<- get_plotting_depths(site_preds_long)
+      
+      # ADD COUNT OF LAKES TO PLOT?
+      filtered_preds <- filter(site_preds_long, depth %in% plot_depths) %>%
+        mutate(depth_class=case_when(
+          depth==plot_depths$plot_depth_surface ~ 'surface',
+          depth==plot_depths$plot_depth_middle ~ 'middle',
+          depth==plot_depths$plot_depth_bottom ~ 'bottom'
+        ), .before=depth)
+      return(filtered_preds)
+    })
+  
+  density_plot <- ggplot(glm_preds) + 
+    geom_density(aes(x=temperature,color=depth_class)) + 
+    facet_grid(gcm~period) +
+    labs(title = paste('Distribution of temperatures across depth classes, by time period and GCM', 
+                       sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n'))
+  
+  ggsave(filename=outfile, plot=density_plot, dpi=300, width=10, height=6)
+  return(outfile)
+}
 
-model_runs <- tibble(
-  output_file = output_files,
-  gcm = stringr::str_extract(output_file, paste(gcm_names,collapse="|")),
-  lake_id = stringr::str_extract(output_file, paste(lake_ids,collapse="|")))
+plot_temp_violin_gcms <- function(run_groups, outfile) {
+  glm_preds <- run_groups %>%
+    group_by(site_id, gcm) %>%
+    group_modify(~ {
+      site_meteo_files <- .x$raw_meteo_fl
+      site_export_files <- .x$export_fl
+      site_preds <- get_site_preds(site_meteo_files, site_export_files)
+      
+      site_preds_long <- munge_long(site_preds)
+      
+      plot_depths<- get_plotting_depths(site_preds_long)
+      
+      # ADD COUNT OF LAKES TO PLOT?
+      filtered_preds <- filter(site_preds_long, depth %in% plot_depths) %>%
+        mutate(depth_class=case_when(
+          depth==plot_depths$plot_depth_surface ~ 'surface',
+          depth==plot_depths$plot_depth_middle ~ 'middle',
+          depth==plot_depths$plot_depth_bottom ~ 'bottom'
+        ), .before=depth)
+      return(filtered_preds)
+    })
+  
+  violin_plot <- ggplot(glm_preds) + 
+    geom_violin(aes(x=depth_class, y=temperature,color=depth_class)) + 
+    facet_grid(period~gcm) +
+    labs(title = paste('Distribution of temperatures across depth classes, by gcm and time period', 
+                       sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n'))
+  
+  ggsave(filename=outfile, plot=violin_plot, dpi=300, width=15, height=6)
+  return(outfile)
+}
+
+get_site_preds <- function(site_meteo_files, site_export_files) {
+  site_preds <- purrr::map2_df(site_meteo_files, site_export_files, function(raw_meteo_fl, export_file) {
+    # Define time period begin and end dates from raw meteo_fl
+    meteo_data <- arrow::read_feather(raw_meteo_fl, col_select = "time")
+    begin <- min(meteo_data$time)
+    end <- max(meteo_data$time)
+    # read in data for that time period and truncate
+    arrow::read_feather(export_file) %>%
+      filter(time >= as.Date(begin) & time <= as.Date(end))
+  }) %>%
+    mutate(date = as.Date(time), .before=1) %>%
+    select(-time) %>%
+    mutate("month" = month(date),
+           "year"= year(date), 
+           "doy"= yday(date),
+           "decade" = sprintf('%ss', year - (year %% 10)),
+           "period" = sprintf('%s - %s', year - (year %% 20), (year - (year %% 20)) + 20),
+           .after=date) %>%
+    filter(!(year == 2000)) %>%
+    select(date, month, year, decade, period, ice, everything())
+  
+  return(site_preds)
+}
+
+plot_temp_density_months <- function(run_groups, outfile) {
+  glm_preds <- run_groups %>%
+    group_by(site_id, gcm) %>%
+    group_modify(~ {
+      site_meteo_files <- .x$raw_meteo_fl
+      site_export_files <- .x$export_fl
+      site_preds <- get_site_preds(site_meteo_files, site_export_files)
+      
+      site_preds_long <- munge_long(site_preds)
+
+      plot_depths<- get_plotting_depths(site_preds_long)
+      
+      # ADD COUNT OF LAKES TO PLOT
+      # FURTHER SPLIT PLOT BY TIME OF YEAR, OR MONTH?
+      filtered_preds <- filter(site_preds_long, depth %in% plot_depths) %>%
+        mutate(depth_class=case_when(
+          depth==plot_depths$plot_depth_surface ~ 'surface',
+          depth==plot_depths$plot_depth_middle ~ 'middle',
+          depth==plot_depths$plot_depth_bottom ~ 'bottom'
+        ), .before=depth) %>%
+      return(filtered_preds)
+    })
+  
+  # filter to only even months
+  # and get mean temperature prediction across GCMs
+  glm_preds <- glm_preds %>%
+    filter(month %% 2 == 0) %>% 
+    group_by(site_id, date, month, period, depth_class, depth) %>% 
+    summarize(mean_temperature=mean(temperature))
+
+  density_plot <- ggplot(glm_preds) + 
+    geom_histogram(aes(x=mean_temperature,color=depth_class)) + 
+    facet_grid(month~period) +
+    labs(title = paste('Distribution of temperatures across depth classes, by time period and (even) months', 
+                       sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n'))
+  browser()
+  ggsave(filename=outfile, plot=density_plot, dpi=300, width=10, height=6)
+  return(outfile)
+}
+
+plot_temp_violin_months <- function(run_groups, outfile) {
+  glm_preds <- run_groups %>%
+    group_by(site_id, gcm) %>%
+    group_modify(~ {
+      site_meteo_files <- .x$raw_meteo_fl
+      site_export_files <- .x$export_fl
+      site_preds <- get_site_preds(site_meteo_files, site_export_files)
+      
+      site_preds_long <- munge_long(site_preds)
+      
+      plot_depths<- get_plotting_depths(site_preds_long)
+      
+      # ADD COUNT OF LAKES TO PLOT
+      # FURTHER SPLIT PLOT BY TIME OF YEAR, OR MONTH?
+      filtered_preds <- filter(site_preds_long, depth %in% plot_depths) %>%
+        mutate(depth_class=case_when(
+          depth==plot_depths$plot_depth_surface ~ 'surface',
+          depth==plot_depths$plot_depth_middle ~ 'middle',
+          depth==plot_depths$plot_depth_bottom ~ 'bottom'
+        ), .before=depth) %>%
+        return(filtered_preds)
+    })
+  
+  # filter to only even months
+  # and get mean temperature prediction across GCMs
+  glm_preds <- glm_preds %>%
+    filter(month %% 2 == 0) %>% 
+    group_by(site_id, date, month, period, depth_class, depth) %>% 
+    summarize(mean_temperature=mean(temperature))
+  
+  violin_plot <- ggplot(glm_preds) + 
+    geom_violin(aes(x=depth_class, y=mean_temperature,color=depth_class)) + 
+    facet_grid(period~month) +
+    labs(title = paste('Distribution of temperatures across depth classes, by (even) months and time period', 
+                       sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n'))
+  
+  ggsave(filename=outfile, plot=violin_plot, dpi=300, width=15, height=6)
+  return(outfile)
+
+}
+
+get_plotting_depths <- function(predictions_long) {
+  n_lambda <- 1
+  depth_scores <- predictions_long %>% group_by(depth) %>% tally() %>%
+    mutate(
+      n_score = n_lambda * n/max(n),
+      surface_score = n_score - abs(depth - 0),
+      middle_score = n_score - abs(depth - max(depth)/3),
+      bottom_score = n_score - abs(depth - max(depth))
+    )
+  depths <- c(
+    surface = depth_scores$depth[which.max(depth_scores$surface_score)],
+    middle = depth_scores$depth[which.max(depth_scores$middle_score)],
+    bottom = depth_scores$depth[which.max(depth_scores$bottom_score)])
+  
+  plot_depth_surface = unname(depths['surface'])
+  plot_depth_middle = unname(depths['middle'])
+  plot_depth_bottom = unname(depths['bottom'])-0.5
+  return(list(plot_depth_surface=plot_depth_surface, plot_depth_middle=plot_depth_middle, plot_depth_bottom=plot_depth_bottom))
+}
+
+# output_files <- list.files('2_run/out/Denali','*.feather')
+# tar_load(p1_gcm_names)
+# lake_ids <- c('nhdhr_91597227','nhdhr_72863673','nhdhr_59724783','nhdhr_47719227','nhdhr_45361793',
+#               'nhdhr_166867643','nhdhr_120020627','nhdhr_120018623','nhdhr_114542327','nhdhr_114539927')
+# gcm_names <- p1_gcm_names 
+
+# model_runs <- tibble(
+#   output_file = output_files,
+#   gcm = stringr::str_extract(output_file, paste(gcm_names,collapse="|")),
+#   lake_id = stringr::str_extract(output_file, paste(lake_ids,collapse="|")))
 
 munge_long <- function(input_wide) {
   input_long <- input_wide %>%
@@ -35,8 +245,16 @@ get_glm_preds <- function(site, gcm, format='long') {
 }
 
 
-plot_20yr_preds <- function(model_run) {
-  glm_preds <- arrow::read_feather(sprintf('2_run/out/Denali/%s',model_run$output_file)) %>%
+plot_20yr_preds <- function(run_group, outfile_template) {
+  glm_preds <- purrr::map2_df(run_group$raw_meteo_fl, run_group$export_fl, function(raw_meteo_fl, export_file) {
+    # Define time period begin and end dates from raw meteo_fl
+    meteo_data <- arrow::read_feather(raw_meteo_fl, col_select = "time")
+    begin <- min(meteo_data$time)
+    end <- max(meteo_data$time)
+    # read in data for that time period and truncate
+    arrow::read_feather(export_file) %>%
+      filter(time >= as.Date(begin) & time <= as.Date(end))
+  }) %>%
     mutate(date = as.Date(time), .before=1) %>%
     select(-time)
   
@@ -85,7 +303,7 @@ plot_20yr_preds <- function(model_run) {
     mutate("decade" = sprintf('%ss', year - (year %% 10))) %>%
     mutate("period" = sprintf('%s - %s', year - (year %% 20), (year - (year %% 20)) + 20)) %>%
     filter(!(year == 2000)) %>%
-    select(date, year, doy, decade, period, depth_column_surface, depth_column_middle, depth_column_bottom)
+    select(date, year, doy, decade, period, !!depth_column_surface, !!depth_column_middle, !!depth_column_bottom)
   
   glm_plot <- glm_subset %>%
     gather(depth, temperature, colnames(glm_subset)[6]:colnames(glm_subset)[ncol(glm_subset)]) %>%
@@ -105,7 +323,7 @@ plot_20yr_preds <- function(model_run) {
     geom_point(data = glm_preds_ice, aes(x=doy, y=temperature, color='c_gcm_glm_ice'), size=0.1, alpha=0.05, pch=16) +
     scale_color_manual(values = c("cornflowerblue","midnightblue", "maroon4"), labels = c('GLM preds','mean of GLM preds', 'GLM ice'), guide= guide_legend(override.aes = list(alpha = c(1,1,1), linetype = c('solid','solid','blank'), shape=c(NA,NA,16), size=c(0.5,0.5,0.5)))) +
     facet_grid(depth ~ period, labeller=labeller(.cols=label_both)) +
-    labs(title= paste(sprintf("%s", model_run$lake_id), sprintf('GCM: %s', model_run$gcm), sep='\n'),  y="Temperature (°C)") +
+    labs(title= paste(sprintf("%s", run_group$lake_id), sprintf('GCM: %s', run_group$gcm), sep='\n'),  y="Temperature (°C)") +
     theme_bw() +
     theme(
       panel.border = element_blank(),
@@ -118,15 +336,17 @@ plot_20yr_preds <- function(model_run) {
       legend.title = element_blank(),
       legend.position = "top"
     )
-  # return(doy_plot)
-  plot_filepath <- sprintf('4_visualize/out/20yr_preds_ice_%s_%s.png', model_run$lake_id, model_run$gcm)
-  ggsave(filename=plot_filepath, plot=doy_plot, dpi=300, width=10, height=6)
+  
+  outfile <- sprintf(outfile_template, unique(run_group$site_id), unique(run_group$gcm))
+  
+  ggsave(filename=outfile, plot=doy_plot, dpi=300, width=10, height=6)
+  return(outfile)
 }
-plot_20yr_preds(test)
-purrr::pmap(model_runs, function(...) {
-  model_run <- tibble(...) 
-  plot_20yr_preds(model_run)
-})
+# plot_20yr_preds(test)
+# purrr::pmap(model_runs, function(...) {
+#   model_run <- tibble(...) 
+#   plot_20yr_preds(model_run)
+# })
 
 
 
@@ -232,10 +452,10 @@ plot_20yr_average_preds <- function(selected_site) {
   ggsave(filename=plot_filepath, plot=doy_plot, dpi=300, width=10, height=6)
 }
 
-plot_20yr_average_preds(lake_ids[1])
-purrr::map(lake_ids, function(lake_id) {
-  plot_20yr_average_preds(lake_id)
-})
+# plot_20yr_average_preds(lake_ids[1])
+# purrr::map(lake_ids, function(lake_id) {
+#   plot_20yr_average_preds(lake_id)
+# })
 
 plot_20yr_average_profiles <- function(selected_site) {
   
@@ -290,7 +510,7 @@ plot_20yr_average_profiles <- function(selected_site) {
   plot_filepath <- sprintf('4_visualize/out/20yr_profiles_%s.png', selected_site)
   ggsave(filename=plot_filepath, plot=doy_plot, dpi=300, width=10, height=6)
 }
-plot_20yr_average_profiles(lake_ids[1])
-purrr::map(lake_ids, function(lake_id) {
-  plot_20yr_average_profiles(lake_id)
-})
+# plot_20yr_average_profiles(lake_ids[1])
+# purrr::map(lake_ids, function(lake_id) {
+#   plot_20yr_average_profiles(lake_id)
+# })
