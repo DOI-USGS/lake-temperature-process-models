cc603ee584b52811895749831676b7e2523db137
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-04-01 16:18:22 -0500

    Pull out steps to extract glm preds into indivudal targets, streamline plotting code

--- 4_visualize/src/plot_data_utility_fxns.R
+++ 4_visualize/src/plot_data_utility_fxns.R
@@ -1,43 +1,29 @@
 #' @title Get site-specifc GLM predictions
 #' @description function to pull and combine GLM predictions for a single site
-#' for all 3 simulation periods based on the input meteo file and export file 
-#' vectors provided - may be pulling predictions for a single lake-gcm combo, 
+#' for all 3 simulation periods - may be pulling predictions for a single lake-gcm combo, 
 #' or predictions for all GCMS for a single lake.
-#' @param site_id The id for the current lake
-#' @param site_meteo_files the filepaths for the raw meteo data. The meteo data
-#' is used to determine the correct begin and end dates for the predictions and
-#' exclude predictions for any burn-in/burn-out periods.
-#' @param export_files the filepaths for the GLM predictions for the current lake
-#' for each time period and each GCM, if predictions for multiple GCMs are being 
-#' read in.
-#' @param run_gcm_list A list of GCM names that corresponds to the `site_meteo_files` 
-#' and `export_files` that are being read in. Depending on whether predictions are being
-#' read in for a single lake-gcm combo, or for all GCMs for a single lake, it may 
-#' be a list of the same GCM name repeated, or a list that includes all 6 GCM names.
+#' @result_group a single group of model runs for which to load results
 #' @return A munged dataframe of GLM predictions that includes columns for date, month, 
-#' year, DOY, decade, period, gcm, ice, hice (ice thickness), evap (evaporation),
+#' year, DOY, time period, gcm, ice, hice (ice thickness), evap (evaporation),
 #' n_layers (number of simulated layers), and temperature predictions at 0.5-meter intervals
-get_site_preds <- function(site_id, site_meteo_files, site_export_files, run_gcm_list) {
-  site_preds <- purrr::pmap_df(list(site_meteo_files, site_export_files, run_gcm_list), function(raw_meteo_fl, export_file, gcm) {
-    # Define time period begin and end dates from raw meteo_fl
-    meteo_data <- arrow::read_feather(raw_meteo_fl, col_select = "time")
-    begin <- min(meteo_data$time)
-    end <- max(meteo_data$time)
-    # read in data for that time period and truncate
-    arrow::read_feather(export_file) %>%
-      filter(time >= as.Date(begin) & time <= as.Date(end)) %>%
-      mutate(gcm = gcm)
-  }) %>%
-    mutate(site_id = site_id, date = as.Date(time), .before=1) %>%
+get_site_preds <- function(result_group) {
+  
+  site_preds <- purrr::pmap_df(result_group, function(...) {
+    current_results <- tibble(...)
+    # read in data for that time period and truncate the
+    # predictions based on the defined start and end dates
+    arrow::read_feather(current_results$export_fl) %>%
+      filter(time >= current_results$gcm_start_date & time <= current_results$gcm_end_date) %>%
+      mutate(gcm = current_results$gcm, time_period = current_results$time_period)
+  }) %>%  
+    mutate(site_id = unique(result_group$site_id), date = as.Date(time)) %>%
     select(-time) %>%
-    mutate("month" = month(date),
-           "year"= year(date), 
-           "doy"= yday(date),
-           "decade" = sprintf('%ss', year - (year %% 10)),
-           "period" = sprintf('%s - %s', year - (year %% 20), (year - (year %% 20)) + 20),
+    mutate(month = month(date),
+           year = year(date), 
+           doy = yday(date),
+           period = gsub('_',' - ', time_period),
            .after=date) %>%
-    filter(!(year == 2000)) %>% #Predictions for this year are incomplete and fall in a different 20-year period, so exclude
-    select(site_id, date, month, year, doy, decade, period, gcm, ice, hice, evap, n_layers, everything()) # Place the temperature prediction columns last
+    select(site_id, date, month, year, doy, period, gcm, ice, hice, evap, n_layers, everything())
   
   return(site_preds)
 }
@@ -52,18 +38,16 @@ get_site_preds <- function(site_id, site_meteo_files, site_export_files, run_gcm
 #' glm_success==TRUE for all runs in that group.
 #' @return A munged dataframe of surface, middle, and bottom temperature GLM
 #' predictions for all successfully simulated lakes. The munge table includes 
-#' the columns site_id, gcm, date, month, year DOY, decade, period, gcm, 
+#' the columns site_id, gcm, date, month, year DOY, time period, gcm, 
 #' ice, hice (ice thickness), evap (evaporation), n_layers (number of simulated 
 #' layers), depth_class (surface, middle, or bottom), depth, and temperature 
 get_surface_middle_bottom_preds <- function(run_groups) {
   surface_middle_bottom_preds <- run_groups %>%
     group_by(site_id) %>%
     group_modify(~ {
-      site_meteo_files <- .x$raw_meteo_fl
-      site_export_files <- .x$export_fl
-      run_gcm_list <- .x$gcm
       # Pull predictions for each site
-      site_preds <- get_site_preds(site_meteo_files, site_export_files, run_gcm_list)
+      site_preds <- get_site_preds(.x) %>%
+        select(-site_id) # drop since returned dataframe cannot contain the grouping variable
       
       # Munge to long format
       site_preds_long <- munge_long(site_preds)
@@ -78,7 +62,7 @@ get_surface_middle_bottom_preds <- function(run_groups) {
           depth==plot_depths$plot_depth_bottom ~ 'bottom'
         ), .before=depth)
       return(filtered_preds)
-    })
+    }, .keep = TRUE ) # keep grouping variable (site_id) in .x, since needed by `get_site_preds()`
   return(surface_middle_bottom_preds)
 }
 
--- 4_visualize/src/plot_results.R
+++ 4_visualize/src/plot_results.R
@@ -1,61 +1,34 @@
 #' @title Create a violin chart of predicted surface, middle, and bottom 
-#' temperatures in all lakes, across GCMs
+#' temperatures in all lakes for all GCMs
 #' @description Plot the distribution of predicted temperatures
 #' across all lakes, broken out by depth class (surface, middle or bottom),
-#' gcm, and time period. The output plot is faceted by GCM and by period
-#' @param run_groups The `p2_glm_uncalibrated_run_groups` grouped version 
-#' of the `p2_glm_uncalibrated_runs` output tibble subset to the site_id, 
-#' gcm, time_period, raw_meteo_fl, export_fl, and export_fl_hash columns 
-#' and grouped by site_id and gcm. Then filtered to only groups for which 
-#' glm_success==TRUE for all runs in that group.
+#' by time period. The output plot is faceted either by gcm or by month
+#' (only even months), as set by `faceting_variable`, and by time period
+#' @param glm_preds Long-formatted temperature predictions for every
+#' lake-gcm combo for which model runs for all 3 time periods succeeded,
+#' filtered to only surface, middle, and bottom depths for each lake
+#' @param faceting_variable variable that will set the columns of the faceted 
+#' plot, either 'gcm' or 'month'. The rows for all plots will be time periods.
 #' @param outfile The filepath for the exported png
 #' @return The filepath of the exported png 
-plot_temp_violin_gcms <- function(run_groups, outfile) {
-  # Get surface, middle, and bottom predictions for each lake, across all GCMs
-  glm_preds <- get_surface_middle_bottom_preds(run_groups)
+plot_temp_violin <- function(glm_preds, faceting_variable, outfile) {
+  
+  # If faceting by months...
+  if (faceting_variable == 'month') {
+    # filter GLM preds to only even months
+    glm_preds <- glm_preds %>%
+      filter(month %% 2 == 0)
+  }
   
   # Build plot
+  plot_title <- paste(sprintf('Distribution of temperatures across depth classes, by %s and time period', 
+                              ifelse(faceting_variable=='month', paste0('(even) ', faceting_variable, 's'), faceting_variable)), 
+                        sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n')
   violin_plot <- ggplot(glm_preds) + 
     geom_violin(aes(x=depth_class, y=temperature,color=depth_class), trim=FALSE) + 
-    facet_grid(period~gcm) +
-    labs(title = paste('Distribution of temperatures across depth classes, by gcm and time period', 
-                       sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n'))
-  
-  # Save plot
-  ggsave(filename=outfile, plot=violin_plot, dpi=300, width=15, height=6)
-  return(outfile)
-}
-
-#' @title Create a violin chart of predicted surface, middle, and bottom 
-#' temperatures in all lakes for all GCMs, across (even) months
-#' @description Plot the distribution of predicted temperatures
-#' across all lakes, broken out by depth class (surface, middle or bottom),
-#' by month (only even months are plotted currently), and time period.
-#' The output plot is faceted by month and by period
-#' @param run_groups The `p2_glm_uncalibrated_run_groups` grouped version 
-#' of the `p2_glm_uncalibrated_runs` output tibble subset to the site_id, 
-#' gcm, time_period, raw_meteo_fl, export_fl, and export_fl_hash columns 
-#' and grouped by site_id and gcm. Then filtered to only groups for which 
-#' glm_success==TRUE for all runs in that group.
-#' @param outfile The filepath for the exported png
-#' @return The filepath of the exported png 
-plot_temp_violin_months <- function(run_groups, outfile) {
-  # Get surface, middle, and bottom predictions for each lake, across all GCMs
-  glm_preds <- get_surface_middle_bottom_preds(run_groups)
-  
-  # filter to only even months
-  # and get mean temperature predictions across GCMs
-  glm_preds <- glm_preds %>%
-    filter(month %% 2 == 0) %>% 
-    group_by(site_id, date, month, period, depth_class, depth) %>% 
-    summarize(mean_temperature=mean(temperature, na.rm=TRUE))
-  
-  # Build plot
-  violin_plot <- ggplot(glm_preds) + 
-    geom_violin(aes(x=depth_class, y=mean_temperature,color=depth_class), trim=FALSE) + 
-    facet_grid(period~month) +
-    labs(title = paste('Distribution of temperatures across depth classes, by (even) months and time period', 
-                       sprintf('number of lakes: %s', length(unique(glm_preds$site_id))), sep='\n'))
+    facet_grid(period~get(faceting_variable)) +
+    labs(title = plot_title) +
+    theme(axis.title.x=element_blank())
   
   # Save plot
   ggsave(filename=outfile, plot=violin_plot, dpi=300, width=15, height=6)
@@ -63,117 +36,41 @@ plot_temp_violin_months <- function(run_groups, outfile) {
 }
 
 #' @title Plot predicted surface, middle, and bottom temperatures in a 
-#' given lake, for a given GCM
-#' @description For a single lake, a single GCM and all 3 time periods, 
+#' given lake, for a single GCM or for all 6 GCMs
+#' @description For a single lake, all 3 time periods, and 1 or 6 GCMs,
 #' plot surface, middle, and bottom temperature predictions in each year, 
 #' by DOY, as well as the average GLM prediction for each DOY in each 
 #' 20-year time period. Also plot whether or not the model predicted ice 
-#' on a given DOY. The output plot is faceted by period and by depth class
-#' @param run_group a single group from `p4_subset_run_groups`, which
-#' is a filtered version of the `p2_glm_uncalibrated_run_groups` tibble, 
-#' which includes columns for site_id, gcm, time_period, raw_meteo_fl, 
-#' export_fl, and export_fl_hash for *fully successful* runs. To limit the 
-#' number of lake-specific plots that are generated, the `p4_subset_run_groups`
-#' was created by filtering `p2_glm_uncalibrated_run_groups` to site ids 
-#' within the manually specified `p4_plot_site_ids` vector, retaining the 
-#' grouping by site_id and gcm. The function maps over these groups.
-#' @param outfile_template The template filepath for the exported png, which
-#' is customized with the site_id and the name of the GCM for which predictions
-#' are plotted
-#' @return The filepath of the exported png 
-plot_20yr_gcm_preds_ice <- function(run_group, outfile_template) {
-  # Get glm preds for the current lake (wide format)
-  glm_preds <- get_site_preds(run_group$raw_meteo_fl, run_group$export_fl, run_group$gcm)
-  
-  # Pull out ice predictions, and create offset temperature for plotting 
-  glm_preds_ice <- glm_preds %>%
-    select(date, doy, ice, temp_0) %>%
-    rename(temperature = temp_0) %>% # could use actual surface temp
-    mutate(temperature = -1) %>% # artificially set to -1 so doesn't block surface temp pred
-    mutate(depth = 0) %>%
-    filter(ice ==TRUE) %>%
-    arrange(date, depth)
-
-  # Munge long
-  glm_preds_long <- munge_long(glm_preds)
-  
-  # Determine surface, middle, and bottom depths for plotting
-  plot_depths <- get_plotting_depths(glm_preds_long)
-
-  # Filter predictions to plotting depths
-  glm_plot <- glm_preds_long %>%
-    filter(depth %in% plot_depths)
-  
-  # Get mean of GLM preds across all years
-  glm_mean <- glm_plot %>%
-    group_by(depth, doy, period) %>%
-    summarize(mean_temp = mean(temperature, na.rm=TRUE))
-  
-  # Build plot  
-  doy_plot <- ggplot()+
-    geom_line(data=glm_plot, aes(x=doy, y=temperature, color='a_gcm_glm'), size=0.5, alpha=0.2, linetype='solid') +
-    geom_line(data=glm_mean, aes(x=doy, y=mean_temp, color="b_gcm_glm_mean"), size=0.5, alpha=1, linetype='solid') +
-    geom_point(data = glm_preds_ice, aes(x=doy, y=temperature, color='c_gcm_glm_ice'), size=0.1, alpha=0.05, pch=16) +
-    scale_color_manual(values = c("cornflowerblue","midnightblue", "maroon4"), labels = c('GLM preds','mean of GLM preds', 'GLM ice'), guide= guide_legend(override.aes = list(alpha = c(1,1,1), linetype = c('solid','solid','blank'), shape=c(NA,NA,16), size=c(0.5,0.5,0.5)))) +
-    facet_grid(depth ~ period, labeller=labeller(.cols=label_both)) +
-    labs(title= paste(sprintf("%s", unique(run_group$site_id)), sprintf('GCM: %s', unique(run_group$gcm)), sep='\n'),  y="Temperature (\u00b0C)") +
-    theme_bw() +
-    theme(
-      panel.border = element_blank(),
-      strip.text = element_text(size=14),
-      axis.text = element_text(size=12),
-      plot.title = element_text(size=16, face="bold", hjust=0.5),
-      axis.title.x = element_text(size=16),
-      axis.title.y = element_text(size=16),
-      legend.text = element_text(size=14),
-      legend.title = element_blank(),
-      legend.position = "top"
-    )
-  
-  # Save plot
-  outfile <- sprintf(outfile_template, unique(run_group$site_id), unique(run_group$gcm))
-  ggsave(filename=outfile, plot=doy_plot, dpi=300, width=10, height=6)
-  return(outfile)
-}
-
-#' @title Plot predicted surface, middle, and bottom temperatures in a 
-#' given lake, for all 6 GCMs
-#' @description For a single lake, all 6 GCMs and all 3 time periods, 
-#' plot surface, middle, and bottom temperature predictions in each year, 
-#' by DOY, as well as the average GLM prediction for each DOY in each 
-#' 20-year time period. Also plot whether or not the model predicted ice 
-#' on a given DOY for any GCM or for all 6 GCMs. The output plot is 
-#' faceted by period and by depth class
-#' @param lake_group a single group from `p4_subset_lake_groups`, which
-#' is a filtered version of the `p2_glm_uncalibrated_lake_groups` tibble, 
-#' which includes columns for site_id, gcm, time_period, raw_meteo_fl, 
-#' export_fl, and export_fl_hash for *fully successful* runs. To limit the 
-#' number of lake-specific plots that are generated, the `p4_subset_lake_groups`
-#' was created by filtering `p2_glm_uncalibrated_lake_groups` to site ids 
-#' within the manually specified `p4_plot_site_ids` vector, retaining the 
-#' grouping by site_id. The function maps over these groups.
+#' on a given DOY for any GCM (or, if plotting all GCMS, for all 6 GCMs). 
+#' The output plot is faceted by time period and by depth class
+#' @param glm_preds_long Long-formatted temperature predictions for the
+#' current lake, for a single GCM or for all GCMs (6 values for each depth 
+#' on each calendar date) if `all_gcms` is TRUE
+#' @param glm_mean_long Long-formatted mean temperature predictions for
+#' each DOY across all years, for a single GCM or for all GCMs (single 
+#' value for each depth on each doy in each time period) if `all_GCMs` is
+#' TRUE
+#' @param all_gcms logical parameter indicating whether or not the plot
+#' includes data for all 6 GCMs
 #' @param outfile_template The template filepath for the exported png, which
 #' is customized with the site_id
 #' @return The filepath of the exported png 
-plot_20yr_average_preds_ice <- function(site_id, glm_preds, glm_preds_long, glm_mean_long, outfile_template) {
-
-  # Pull out ice predictions, and create offset temperature for plotting
-  glm_preds_ice <- glm_preds %>%
-    select(gcm, year, doy, period, date, ice, temp_0) %>%
-    rename(temperature = temp_0) %>% # could use actual surface temp
-    mutate(temperature = -1) %>% # artificially set to -1 so doesn't block surface temp pred
-    mutate(depth = 0) %>%
-    filter(!(year == 2000))
+plot_20yr_preds_ice <- function(glm_preds_long, glm_mean_long, all_gcms, outfile_template) {
   
-  # Get dates when all GCMs predicted ice
-  glm_preds_ice_mean <- glm_preds_ice %>%
+  # Pull out ice predictions, and create offset temperature for plotting
+  glm_preds_ice <- glm_preds_long %>%
+    select(date, year, doy, period, gcm, ice, depth, temperature) %>%
+    filter(depth == 0) %>%
+    mutate(temperature = -1) %>% # could use actual surface temp, but artificially set to -1 so doesn't block surface temp pred
+    filter(ice ==TRUE) # filter to only days when ice was present in any year/for any gcm
+  
+  # Get dates when all years (or all years and all GCMs) predicted ice
+  glm_preds_ice_mean <- glm_preds_long %>%
+    select(date, year, doy, period, gcm, ice, depth, temperature) %>%
+    filter(depth == 0) %>%
     group_by(depth, doy, period) %>%
-    summarize(ice_all_gcms = ifelse(sum(ice)==n(), TRUE, FALSE), mean_temp = -2) %>%
-    filter(ice_all_gcms ==TRUE)
-
-  # Filter ice predictions to only those dates when ice was present
-  glm_preds_ice <- glm_preds_ice %>%
-    filter(ice==TRUE)
+    summarize(ice_all = ifelse(sum(ice)==n(), TRUE, FALSE), mean_temp = -2) %>% # could use actual surface temp, but artificially set to -2
+    filter(ice_all == TRUE) # filter to only days when ice was present in all years/for all gcms
   
   # Determine surface, middle, and bottom depths for plotting
   plot_depths <- get_plotting_depths(glm_preds_long)
@@ -184,20 +81,20 @@ plot_20yr_average_preds_ice <- function(site_id, glm_preds, glm_preds_long, glm_
   glm_mean_plot <- glm_mean_long %>%
     filter(depth %in% plot_depths)
   
-  # # Get mean of GLM predictions across all years and all 6 GCMs
-  # glm_mean <- glm_plot %>%
-  #   group_by(depth, doy, period) %>%
-  #   summarize(mean_temp = mean(temperature, na.rm=TRUE))
-  
   # Build plot
+  ice_label <- ifelse(all_gcms == TRUE, 'any GCM ice', 'any year ice')
+  ice_mean_label <- ifelse(all_gcms == TRUE, 'all GCMs ice', 'all years ice')
+  plot_title <- ifelse(all_gcms == TRUE, 
+                  paste(sprintf("%s", unique(glm_preds_long$site_id)), 'GCM: all', sep='\n'),
+                  paste(sprintf("%s", unique(glm_preds_long$site_id)), sprintf('GCM: %s', unique(glm_preds_long$gcm)), sep='\n'))
   doy_plot <- ggplot()+
     geom_line(data=glm_preds_plot, aes(x=doy, y=temperature, color='a_gcm_glm'), size=0.5, alpha=0.2, linetype='solid') +
     geom_line(data=glm_mean_plot, aes(x=doy, y=mean_temp, color="b_gcm_glm_mean"), size=0.5, alpha=1, linetype='solid') +
     geom_point(data = glm_preds_ice, aes(x=doy, y=temperature, color='c_gcm_glm_ice'), size=0.1, alpha=0.2, pch=16) +
     geom_point(data = glm_preds_ice_mean, aes(x=doy, y=mean_temp, color='d_gcm_glm_ice_mean'), size=0.1, alpha=0.3, pch=16) +
-    scale_color_manual(values = c("cornflowerblue","midnightblue", "plum2","maroon4"), labels = c('GLM preds','mean of GLM preds', 'any GCM ice', 'all GCMs ice'), guide= guide_legend(override.aes = list(alpha = c(1,1,1,1), linetype = c('solid','solid','blank','blank'), shape=c(NA,NA,16,16), size=c(0.5,0.5,0.5,0.5)))) +
+    scale_color_manual(values = c("cornflowerblue","midnightblue", "plum2","maroon4"), labels = c('GLM preds','mean of GLM preds', ice_label, ice_mean_label), guide= guide_legend(override.aes = list(alpha = c(1,1,1,1), linetype = c('solid','solid','blank','blank'), shape=c(NA,NA,16,16), size=c(0.5,0.5,0.5,0.5)))) +
     facet_grid(depth ~ period, labeller=labeller(.cols=label_both)) +
-    labs(title= paste(sprintf("%s", site_id), 'GCM: all', sep='\n'),  y="Temperature (\u00b0C)") +
+    labs(title= plot_title,  y="Temperature (\u00b0C)") +
     theme_bw() +
     theme(
       strip.text = element_text(size=14),
@@ -211,17 +108,20 @@ plot_20yr_average_preds_ice <- function(site_id, glm_preds, glm_preds_long, glm_
     )
   
   # Save plot
-  outfile <- sprintf(outfile_template, site_id)
+  outfile <- ifelse(all_gcms == TRUE, 
+         sprintf(outfile_template, unique(glm_preds_long$site_id)),
+         sprintf(outfile_template, unique(glm_preds_long$site_id), unique(glm_preds_long$gcm)))
+  
   ggsave(filename=outfile, plot=doy_plot, dpi=300, width=10, height=6)
   return(outfile)
 }
 
-#' @title Plot predicted surface, middle, and bottom temperatures in a 
-#' given lake, for all 6 GCMs
+#' @title Plot predicted temperature profiles in a given lake, for all
+#' 6 GCMs, across all years, for 4 specified dates
 #' @description For a single lake, all 6 GCMs and all 3 time periods, 
-#' plot temperature profiles for 4 specified DOY in each year, 
-#' as well as the average GLM profile for each of the 4 DOY within each 
-#' 20-year time period. The output plot is faceted by DOY and by period
+#' plot temperature profiles for 4 specified DOY in each year, as well
+#' as the average GLM profile for each of the 4 DOY within each 20-year
+#' time period. The output plot is faceted by DOY and by time period
 #' @param site_id The id for the current lake
 #' @param glm_preds_long Long-formatted temperature predictions for the
 #' current lake, for all GCMs (6 values for each depth on each calendar
