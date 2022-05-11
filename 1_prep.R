5584406dae66e72ace1e653d8635af1f7c993b00
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-05-11 18:35:24 -0500

    Set up NLDAS nmls and model config

--- 1_prep.R
+++ 1_prep.R
@@ -3,11 +3,8 @@ source('1_prep/src/build_model_config.R')
 source('1_prep/src/munge_nmls.R')
 
 p1 <- list(
-  # Define mapping variables
-  tar_target(p1_gcm_names, c('ACCESS', 'GFDL', 'CNRM', 'IPSL', 'MRI', 'MIROC5')),
-  tar_target(p1_gcm_time_periods, c('1981_2000', '2040_2059', '2080_2099')),
   
-  ### Pull in files from lake-temperature-model-prep
+  ##### Pull in files from lake-temperature-model-prep #####
   # TODO - transfer to Denali using globus
   # list of lake-specific attributes for nml modification
   # file copied from lake-temperature-model-prep repo
@@ -20,7 +17,17 @@ p1 <- list(
   tar_target(p1_lake_cell_tile_xwalk_df, 
              readr::read_csv(p1_lake_cell_tile_xwalk_csv, col_types=cols()) %>%
                filter(site_id %in% p1_nml_site_ids) %>%
-               arrange(site_id)),
+               arrange(site_id) %>% #),
+               filter(site_id %in% c('nhdhr_105567868','nhdhr_105569506', 'nhdhr_105569520', 'nhdhr_114336097', 'nhdhr_120019185','nhdhr_114544667'))),
+  # Pull vector of site ids
+  tar_target(p1_site_ids, p1_lake_cell_tile_xwalk_df %>% pull(site_id)),
+  # Subset nml list
+  tar_target(p1_nml_list, readr::read_rds(p1_nml_list_rds)[p1_site_ids]),
+  
+  ##### GCM model set up #####
+  # Define mapping variables
+  tar_target(p1_gcm_names, c('ACCESS', 'GFDL', 'CNRM', 'IPSL', 'MRI', 'MIROC5')),
+  tar_target(p1_gcm_time_periods, c('1981_2000', '2040_2059', '2080_2099')),
   # NetCDF files with munged GCM driver data (one per GCM)
   # files copied from lake-temperature-model-prep repo
   tar_target(p1_gcm_ncs, 
@@ -31,8 +38,7 @@ p1 <- list(
              format = 'file',
              pattern = map(p1_gcm_names)),
 
-  # Pull vectors of site ids and data cell numbers
-  tar_target(p1_site_ids, p1_lake_cell_tile_xwalk_df %>% pull(site_id)),
+  # Pull vectors data cell numbers
   tar_target(p1_cell_nos, unique(p1_lake_cell_tile_xwalk_df %>% pull(data_cell_no))),
 
   # Specify length of desired burn-in and burn-out periods, in days
@@ -64,9 +70,61 @@ p1 <- list(
   # (subset to p1_site_ids), then tell `targets` to think of that list as an iterable list
   tar_target(p1_glm_template_nml, '1_prep/in/glm3_template.nml', format = 'file'),
   tar_target(p1_nml_objects,
-             munge_nmls(nml_list_rds = p1_nml_list_rds,
-                        site_ids = p1_site_ids,
+             munge_nmls(nml_list_rds = p1_nml_list_rds, # could pass p1_nml_list instead
+                        site_ids = p1_site_ids, # and then wouldn't need to pass site_ids
                         base_nml = p1_glm_template_nml),
+             packages = c('glmtools'),
+             iteration = 'list'),
+  
+  ##### NLDAS model set up #####
+  tar_target(p1_nldas_time_period, c('1980_2021')),
+  # # track unique NLDAS files
+  # # length(p1_nldas_csvs) = # of unique NLDAS files associated with p1_site_ids
+  # tar_target(p1_nldas_csvs,
+  #            {
+  #              nldas_files <- c()
+  #              for (site_id in p1_site_ids) {
+  #                site_nml <- p1_nml_list[[site_id]]
+  #                site_nldas_file <- file.path('1_prep/in/NLDAS_GLM_csvs', site_nml$meteo_fl)
+  #                if (!(site_nldas_file %in% nldas_files)) {
+  #                  nldas_files <- c(nldas_files, site_nldas_file)
+  #                }
+  #              }
+  #              return(nldas_files)
+  #            },
+  #            format = 'file'
+  # ),
+  # track NLDAS files for each site
+  # length(p1_nldas_csvs) = length(site_ids)
+  tar_target(p1_nldas_csvs,
+             {
+               site_nml <- p1_nml_list[[p1_site_ids]]
+               site_nldas_file <- file.path('1_prep/in/NLDAS_GLM_csvs', site_nml$meteo_fl)
+               return(site_nldas_file)
+             },
+             format = 'file',
+             pattern = map(p1_site_ids)
+             ),
+  
+  # Define model start and end dates and note start of
+  # burn-in and end of burn-out based on extent of NLDAS data
+  # (using 1/2/1979 - 12/31/1979 for burn-in, and 1/1/2021 - 
+  # 4/11/2021 for burn-out)
+  tar_target(
+    p1_nldas_dates,
+    munge_nldas_dates(p1_nldas_csvs, p1_nldas_time_period)),
+  
+  # Set up NLDAS model config
+  tar_target(p1_nldas_model_config,
+             build_nldas_model_config(p1_site_ids, p1_nldas_csvs, p1_nldas_dates),
+             pattern = map(p1_site_ids, p1_nldas_csvs)
+  ),
+  
+  # Set up nmls for NLDAS model runs
+  tar_target(p1_nldas_nml_objects,
+             munge_model_nmls(nml_list = p1_nml_list,
+                              base_nml = p1_glm_template_nml,
+                              driver_type = 'nldas'),
              packages = c('glmtools'),
              iteration = 'list')
 )
--- 1_prep/src/build_model_config.R
+++ 1_prep/src/build_model_config.R
@@ -49,3 +49,53 @@ build_model_config <- function(meteo_csvs, lake_cell_tile_xwalk, gcm_names, gcm_
     rowwise()
   return(model_config)
 }
+
+build_nldas_model_config <- function(site_id, nldas_csv, nldas_dates) {
+  model_config <- tibble(
+    site_id = site_id, 
+    gcm = 'nldas', #using column name 'gcm' for testing - then will be 'driver_type'
+    meteo_fl = nldas_csv,
+    meteo_fl_hash = tools::md5sum(nldas_csv)
+  ) %>%
+    cbind(nldas_dates) %>%
+    # for now, use column names 'gcm_start_date' and 'gcm_end_date' for testing, - then will be 'driver_start_date'
+    select(site_id, gcm, time_period, gcm_start_date=driver_start_date, gcm_end_date=driver_end_date, 
+           burn_in, burn_in_start, burn_out, burn_out_end, meteo_fl, meteo_fl_hash) #select(site_id, gcm, colnames(nldas_dates), meteo_fl, meteo_fl_hash)
+
+  return(model_config)
+}
+
+#' @title Munge NLDAS dates
+#' @description Function to determine the start and end dates of the
+#' NLDAS time period and document the date when burn-in starts and the date
+#' when burn-out ends, based on the dates of the raw NLDAS meteorological data.
+#' @param nldas_csvs filepaths of the NLDAS csv files
+#' @param nldas_time_period - the user-set NLDAS time period, defined by its 
+#' bracketing years
+#' @return a tibble with a row for each time period, and columns specifying
+#' the start and end date of each time period, as well as the length of
+#' burn in, the burn-in start date, the length of burn-out, and the burn-out
+#' end date for each time period
+munge_nldas_dates <- function(nldas_csvs, nldas_time_period) {
+  model_years <- strsplit(nldas_time_period,'_')[[1]]
+  
+  # Use first csv file, since all have the same date range
+  nldas_meteo <- readr::read_csv(nldas_csvs[1], col_types= cols())
+  
+  nldas_dates <- tibble(
+    time = lubridate::as_date(nldas_meteo$time),
+    time_period = nldas_time_period
+    ) %>%
+    group_by(time_period) %>%
+    # set burn-in start and burn-out end based on extend of NLDAS data
+    summarize(burn_in_start = min(time), burn_out_end = max(time)) %>%
+    mutate(
+      driver_start_date = as.Date(sprintf('%s-01-01', model_years[1])), # set based on user-defined modeling period
+      driver_end_date = as.Date(sprintf('%s-12-31', model_years[2])), # set based on user-defined modeling period
+      # the burn-in period is the period that precedes the first full year of the NLDAS time period
+      burn_in = driver_start_date - burn_in_start,
+      # the burn-out period is the period that follows the final full year of the NLDAS time period
+      burn_out = burn_out_end - driver_end_date)
+  
+  return(nldas_dates)
+}
