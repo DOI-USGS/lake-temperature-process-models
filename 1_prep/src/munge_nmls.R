be6d9ab76948e883481f8a35fb3e6fd56b79577e
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-05-12 14:44:12 -0500

    Remove old nml munging function since not needed

--- 1_prep/src/munge_nmls.R
+++ 1_prep/src/munge_nmls.R
@@ -21,33 +21,6 @@ adjust_depth_args_nml <- function(nml_args) {
   return(nml_edits)
 }
 
-#' @Title Munge default nml lists
-#' @decription Replace the NLDAS filename stored as 'meteo_fl'
-#' with 'NULL', remove used nml parameter 'site_id'
-#' @param nml_list_rds rds file of lake-specific nml parameters
-#' @param site_ids vector of lakes from lake_cell_tile_xwalk
-#' @param base_nml glm3 nml template
-#' @return complete nml objects
-munge_nmls <- function(nml_list_rds, site_ids, base_nml) {
-  nml_list <- readr::read_rds(nml_list_rds)[site_ids]
-  nml_template <- read_nml(base_nml)
-  # create the munged nml objects
-  nml_objs <- purrr::map(nml_list, function(nml) {
-      # make edits to nml depth-related parameters
-      nml <- purrr::list_modify(nml, !!!adjust_depth_args_nml(nml))
-      # set meteo_fl value to NULL
-      nml <- purrr::list_modify(nml, !!!c('meteo_fl' = 'NULL'))
-      # disable evaporation
-      nml <- purrr::list_modify(nml, !!!c('disable_evap' = TRUE))
-      # remove helpful but non-nml values
-      nml <- nml[!(names(nml) %in% c('site_id'))]
-      # merge into base nml, checking arguments along the way
-      nml_obj <- set_nml(nml_template, arg_list = nml)
-    })
-  
-  return(nml_objs)
-}
-
 #' @Title Munge default nml lists for NLDAS runs
 #' @decription Customize glm3 template nml using the values
 #' in p1_nml_list, remove used nml parameter 'site_id'
