fd0626bf1a78346bea4a7117016e50ccb54c71f5
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-05-12 15:22:39 -0500

    Streamline functions for processing output to work for all runs, clean up function documentation

--- 3_extract/src/process_glm_output.R
+++ 3_extract/src/process_glm_output.R
@@ -1,25 +1,22 @@
 #' @title Combine output from GLM model runs
-#' @description function to read in the raw output from the 3 GLM
-#' runs for each fully successful lake-gcm combo (set by grouping of 
-#' `p2_glm_uncalibrated_run_groups`), remove the ice thickness, 
+#' @description function to read in the raw output from successful
+#' GLM runs using GCM or NLDAS drivers, remove the ice thickness, 
 #' evaporation, and n_layers variables (leaving the temperature 
 #' predictions and ice flags), filter that output to exclude the
 #' burn-in and burn-out periods, and save the result as a single 
 #' feather file
-#' @param run_group a single group from the `p2_glm_uncalibrated_run_groups`
-#' grouped version of the `p2_glm_uncalibrated_runs` output tibble subset 
-#' to the site_id, gcm, time_period, gcm_start_date, gcm_end_date, export_fl,  
-#' and export_fl_hash columns and grouped by site_id and gcm. Then filtered  
-#' to only groups for which glm_success==TRUE for all runs in that group. 
-#' The function maps over these groups.
+#' @param run_group a single group of successful model runs. If the
+#' passed runs are GCM runs, this group includes all runs for a lake-gcm combo
+#' (nrows = 3). If the passed runs are NLDAS runs, this group includes all
+#' runs for a given lake (nrows = 1). The function maps over these groups.
 #' @param outfile_template the template for the name of the
 #' final output feather file
 #' @return a single feather file with the output temperature predictions
-#' and ice flags for that lake-gcm combo, filtered to the valid dates 
-#' from each time period
-combine_glm_output <- function(run_group, outfile_template) {
+#' and ice flags for that lake-gcm combo (if GCM runs) or lake (if NLDAS runs), 
+#' filtered to the valid dates (excluding burn-in/out from each time period)
+write_glm_output <- function(run_group, outfile_template) {
   # set filename
-  outfile <- sprintf(outfile_template, unique(run_group$site_id), unique(run_group$gcm))
+  outfile <- sprintf(outfile_template, unique(run_group$site_id), unique(run_group$driver))
   
   # combine into single feather file and write,
   # saving only the temperature predictions and ice flags, and
@@ -32,7 +29,7 @@ combine_glm_output <- function(run_group, outfile_template) {
     # the predictions based on the defined start and end dates
     arrow::read_feather(current_run$export_fl) %>%
       select(-hice, -evap, -n_layers) %>%
-      filter(time >= current_run$gcm_start_date & time <= current_run$gcm_end_date)
+      filter(time >= current_run$driver_start_date & time <= current_run$driver_end_date)
   }) %>% 
     arrow::write_feather(outfile)
   
@@ -43,15 +40,16 @@ combine_glm_output <- function(run_group, outfile_template) {
 #' feather files
 #' @description Generate a tibble with a row for each output file
 #' (unique to each lake-gcm combo) that includes its filename and its 
-#' hash along with the site_id, gcm, the state the lake is in, and the 
+#' hash along with the site_id, driver (gcm_name), the state the lake is in, and the 
 #' GCM spatial_cell_no, spatial_tile_no, data_cell_no, and data_tile_no 
 #' for that lake.
-#' @param run_group a single group from the `p2_glm_uncalibrated_run_groups`
-#' grouped version of the `p2_glm_uncalibrated_runs` output tibble subset 
-#' to the site_id, gcm, time_period, raw_meteo_fl, export_fl, and 
-#' export_fl_hash columns and grouped by site_id and gcm. Then filtered to 
-#' only groups for which glm_success==TRUE for all runs in that group. 
-#' The function maps over these groups.
+#' @param run_group a single group from the `p2_gcm_glm_uncalibrated_run_groups`
+#' grouped version of the `p2_gcm_glm_uncalibrated_runs` output tibble subset 
+#' to the site_id, driver (gcm name), time_period, gcm_start_date, gcm_end_date, 
+#' export_fl, and export_fl_hash columns and grouped by site_id. Then filtered  
+#' to only groups for which glm_success==TRUE for all runs in that group. And
+#' then regrouped by site_id and driver (gcm name). The function maps over 
+#' these groups.
 #' @param output_feather A single feather file name from the list of 
 #' output feather file names returned by `combine_glm_output()`. 
 #' The function maps over these file names.
@@ -61,13 +59,13 @@ combine_glm_output <- function(run_group, outfile_template) {
 #' `data_tile_no`). `data_cell_no` will only differ from `spatial_cell_no` 
 #' for those lakes that fall within gcm cells that are missing data.
 #' @return A tibble with one row per lake-gcm output feather file which 
-#' includes the site_id, gcm, the name of the export feather file, its hash,
-#' the state the lake is in, and the GCM spatial_cell_no, spatial_tile_no, 
+#' includes the site_id, driver (gcm name), the name of the export feather file,
+#' its hash, the state the lake is in, and the GCM spatial_cell_no, spatial_tile_no, 
 #' data_cell_no, and data_tile_no for that lake.
 generate_output_tibble <- function(run_group, output_feather, lake_cell_tile_xwalk) {
   export_tibble <- tibble(
       site_id = unique(run_group$site_id),
-      gcm = unique(run_group$gcm),
+      driver = unique(run_group$driver),
       export_fl = output_feather,
       export_fl_hash = tools::md5sum(output_feather)
     ) %>% 
@@ -77,18 +75,13 @@ generate_output_tibble <- function(run_group, output_feather, lake_cell_tile_xwa
 }
 
 #' @title Zip up output from GLM model runs
-#' @description function to zip up the feather files for all lake-gcm
-#' combos according to the spatial_tile_no associated with each lake
-#' @param feather_group A single group of output feather files associated 
-#' with one tile, from the `p3_glm_uncalibrated_output_feather_groups` 
-#' grouped version of the `p3_glm_uncalibrated_output_feather_tibble` 
-#' target grouped by spatial_tile_no. The function maps over these groups.
-#' @param zipfile_template the template for the name of the final zipped
-#' file of feather files, which will be customized with the spatial_tile_no
+#' @description function to zip up output feather files
+#' @param files_to_zip A single group of output feather files. If for GCM
+#' runs, these feather files are associated with one spatial tile. If for
+#' NLDAS runs, these are all NLDAS output files
+#' @param zipfile_out the name of the final zipped file of feather files
 #' @return the name of the zipped file 
-zip_output_files <- function(feather_group, zipfile_template) {
-  files_to_zip <- feather_group$export_fl
-  zipfile_out <- sprintf(zipfile_template, unique(feather_group$spatial_tile_no))
+zip_output_files <- function(files_to_zip, zipfile_out) {
   # In order to use `zip`, you need to be in the same working directory as
   # the files you want to zip up.
   project_dir <- getwd()
