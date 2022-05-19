4e0a36d98c5c273e09461f01c29b8a1013c5c68b
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-05-19 18:30:24 -0500

    Add code to generate NLDAS model evaluation targets

--- 5_evaluate.R
+++ 5_evaluate.R
@@ -8,12 +8,31 @@ p5 <- list(
   
   ###### Prep predictions and observations ######
   
-  # Get NLDAS preds in long format
-  # As-is, model results are read in by `write_glm_output()` in the `3_extract` step
-  # We could modify that function and split the `p3_nldas_glm_uncalibrated_output_feathers`
-  # target into two - one that reads in the output in wide format - `p3_nldas_glm_uncalibrated_output`, 
-  # and a second that writes it to a feather file - `p3_nldas_glm_uncalibrated_output_feathers`
-  # then we could use that first target here to get NLDAS in long format, per https://github.com/USGS-R/mntoha-data-release/blob/main/src/eval_utils.R#L23-L30
+  # Get vector of site_ids for which we have NLDAS output
+  tar_target(p3_nldas_export_site_ids,
+             p2_nldas_glm_uncalibrated_run_groups %>%
+               pull(site_id)),
+  
+  # Prep site observations
+  # filter obs to sites and dates for which we have NLDAS output
+  # And further filter obs to those for sites w/ >= 10 dates with observations
+  tar_target(p5_obs_for_eval,
+             get_eval_obs(p1_obs_feather, p3_nldas_export_site_ids, p1_nldas_dates$driver_start_date, 
+                          p1_nldas_dates$driver_end_date, min_obs_dates = 10)),
+  
+  # Get vector of evaluation sites, based on availability of observations
+  tar_target(p5_eval_sites,
+             p5_obs_for_eval %>% pull(site_id) %>% unique()),
+
+  # Group filtered obs by site, set up tar_group()
+  tar_target(p5_obs_for_eval_groups,
+             p5_obs_for_eval %>%
+               group_by(site_id) %>%
+               tar_group(),
+             iteration = "group"),
+  
+  # Prep NLDAS predictions
+  # Get NLDAS predictions in long format
   tar_target(
     p5_nldas_preds_long,
     munge_long(p3_nldas_glm_uncalibrated_output) %>%
@@ -22,29 +41,7 @@ p5 <- list(
     pattern = map(p3_nldas_glm_uncalibrated_output, p2_nldas_glm_uncalibrated_run_groups)
   ),
   
-  # Prep site observations
-  ### Bring in `7b_temp_merge/out/merged_temp_data_daily.feather` from `lake-temperature-model-prep`
-  ### (would be target in 1_prep.R)
-  # filter obs to sites for which we have NLDAS output
-  ### Use https://github.com/USGS-R/mntoha-data-release/blob/main/src/file_utils.R#L441-L446
-  ### Passing `driver_start_date` and `driver_end_date` from `p1_nldas_dates`
-  # And subset obs to those for sites w/ >= 10 dates with observations
-  ### Use https://github.com/USGS-R/mntoha-data-release/blob/main/src/eval_utils.R#L4-L11
-  tar_target(p5_obs_for_eval,
-             get_eval_obs(p1_obs_temps, min_dates = 10)),
-  
-  # Get vector of evaluation sites
-  tar_target(p5_eval_sites,
-             p5_obs_for_eval %>% pull(site_id) %>% unique()),
-  
-  # Group subsetted obs by site, set up tar_group()
-  tar_target(p5_obs_for_eval_groups,
-             p5_obs_for_eval %>%
-               group_by(site_id) %>%
-               tar_group(),
-             iteration = "group"),
-  
-  # filter long-format NLDAS preds to only evaluation sites
+  # Filter long-format NLDAS preds to only evaluation sites
   tar_target(
     p5_nldas_preds_eval,
     p5_nldas_preds_long %>%
@@ -55,7 +52,7 @@ p5 <- list(
   tar_target(
     p5_nldas_preds_eval_groups,
     p5_nldas_preds_eval %>%
-      group_by(site_id) %>% 
+      group_by(site_id) %>%
       tar_group(),
     iteration = "group"
   ),
@@ -65,11 +62,18 @@ p5 <- list(
   # actual matching (by site): https://github.com/USGS-R/mntoha-data-release/blob/main/src/eval_utils.R#L113-L132
   # map over eval groups (so parallelizable on Tallgrass)
   tar_target(p5_nldas_pred_obs,
-             match_pred_obs(eval_obs = p5_obs_for_eval_groups, eval_preds = p5_nldas_preds_eval_groups),
+             match_pred_obs(eval_obs = p5_obs_for_eval_groups, eval_preds = p5_nldas_preds_eval_groups) %>%
+               select(-tar_group), # drop grouping column
              pattern = map(p5_obs_for_eval_groups, p5_nldas_preds_eval_groups)),
-  
+
   # Write matched predictions to file
-  tar_target(p5_nldas_pred_obs_csv),
+  tar_target(p5_nldas_pred_obs_csv,
+             {
+               outfile <- '5_evaluate/out/nldas_matched_to_observations.csv'
+               readr::write_csv(p5_nldas_pred_obs, outfile)
+               return(outfile)
+             },
+             format = 'file'),
   
   # Prep matched preds for evaluation
   # Add pred_diff column (pred - obs)
@@ -77,7 +81,7 @@ p5 <- list(
   # Set up variables for which bias/accuracy will be calculated
   # Add fields for year, doy, season, temp_bin
   tar_target(p5_nldas_pred_obs_surface_eval,
-             prep_data_for_eval(p5_nldas_pred_obs) 
+             prep_data_for_eval(p5_nldas_pred_obs, selected_depth = 0)
              ),
   
   ###### Assess model bias ######
@@ -93,7 +97,7 @@ p5 <- list(
   # Bias by season
   tar_target(p5_nldas_surface_bias_season,
              calc_bias(p5_nldas_pred_obs_surface_eval, grouping_var = 'season')),
-  
+
   # Bias for specific temperature ranges
   tar_target(p5_nldas_surface_bias_temp,
              calc_bias(p5_nldas_pred_obs_surface_eval, grouping_var = 'temp_bin')),
@@ -101,37 +105,76 @@ p5 <- list(
   ## Plots
   # Bar plot of bias through time, by  year
   tar_target(p5_nldas_surface_bias_year_png,
-             plot_barplot(p5_nldas_surface_bias_year, y_var = 'bias', 
-                          x_var = 'year', depth='surface', 
-                          outfile = '5_evaluation/out/nldas_surface_bias_year.png'),
+             plot_evaluation_barplot(p5_nldas_surface_bias_year, driver= 'NLDAS', 
+                          y_var = 'bias', x_var = 'year', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_bias_year.png'),
              format = 'file'),
   
   # Bar plot of bias through time, by doy
+  tar_target(p5_nldas_surface_bias_doy_png,
+             plot_evaluation_barplot(p5_nldas_surface_bias_doy, driver= 'NLDAS', 
+                          y_var = 'bias', x_var = 'doy', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_bias_doy.png'),
+             format = 'file'),
   
   # Bar plot of bias by season
+  tar_target(p5_nldas_surface_bias_season_png,
+             plot_evaluation_barplot(p5_nldas_surface_bias_season, driver= 'NLDAS', 
+                          y_var = 'bias', x_var = 'season', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_bias_season.png'),
+             format = 'file'),
   
   # Bar plot of bias for 2-degree temperature bins
+  tar_target(p5_nldas_surface_bias_temp_png,
+             plot_evaluation_barplot(p5_nldas_surface_bias_temp, driver= 'NLDAS', 
+                          y_var = 'bias', x_var = 'temp_bin', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_bias_temp.png'),
+             format = 'file'),
   
   ###### Assess model accuracy ######
   
-  # Accuracy through time - year
+  # # Accuracy through time - year
   tar_target(p5_nldas_surface_accuracy_year,
              calc_rmse(p5_nldas_pred_obs_surface_eval, grouping_var = 'year')),
   
   # Accuracy through time - doy
+  tar_target(p5_nldas_surface_accuracy_doy,
+             calc_rmse(p5_nldas_pred_obs_surface_eval, grouping_var = 'doy')),
   
   # Accuracy by season
+  tar_target(p5_nldas_surface_accuracy_season,
+             calc_rmse(p5_nldas_pred_obs_surface_eval, grouping_var = 'season')),
   
   # Accuracy for specific temperature ranges
-  
+  tar_target(p5_nldas_surface_accuracy_temp,
+             calc_rmse(p5_nldas_pred_obs_surface_eval, grouping_var = 'temp_bin')),
   
   ## Plots
   # Bar plot of accuracy through time, by  year
+  tar_target(p5_nldas_surface_accuracy_year_png,
+             plot_evaluation_barplot(p5_nldas_surface_accuracy_year, driver= 'NLDAS', 
+                          y_var = 'rmse', x_var = 'year', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_accuracy_year.png'),
+             format = 'file'),
   
   # Bar plot of accuracy through time, by doy
+  tar_target(p5_nldas_surface_accuracy_doy_png,
+             plot_evaluation_barplot(p5_nldas_surface_accuracy_doy, driver= 'NLDAS', 
+                          y_var = 'rmse', x_var = 'doy', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_accuracy_doy.png'),
+             format = 'file'),
   
   # Bar plot of accuracy by season
+  tar_target(p5_nldas_surface_accuracy_season_png,
+             plot_evaluation_barplot(p5_nldas_surface_accuracy_season, driver= 'NLDAS', 
+                          y_var = 'rmse', x_var = 'season', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_accuracy_season.png'),
+             format = 'file'),
   
   # Bar plot of accuracy for 2-degree temperature bins
-  
+  tar_target(p5_nldas_surface_accuracy_temp_png,
+             plot_evaluation_barplot(p5_nldas_surface_accuracy_temp, driver= 'NLDAS', 
+                          y_var = 'rmse', x_var = 'temp_bin', depth='surface',
+                          outfile = '5_evaluate/out/nldas_surface_accuracy_temp.png'),
+             format = 'file')
 )
--- 5_evaluate/src/eval_utility_fxns.R
+++ 5_evaluate/src/eval_utility_fxns.R
@@ -1,24 +1,146 @@
-# matching fxn
-match_pred_obs <- function() {
+#' @title Prep site observations for evaluation
+#' @description Prep site observations by reading in the feather file of observations,
+#' filtering to sites for which we have model output, and to dates within the model
+#' period. Then subset observations to those for sites with >= `min_obs_dates` to 
+#' create the set of observations to use in model evaluation
+#' @param obs_feather filepath to lake temperature observations file.
+#' Once read in, has columns for site_id, date, temp, source
+#' @param modeled_sites vector of site_ids for which we have model output
+#' @param start_date earliest date for which we want to keep obs
+#' @param end_date latest date for which we want to keep obs
+#' @param min_obs_dates specified minimum number of days for which a site
+#' must have observations to be included in the evaluation set
+#' @return A tibble of temperature observations for evaluation sites, with columns for
+#' site_id, time, depth, and temperature
+get_eval_obs <- function(obs_feather, modeled_sites, start_date, end_date, min_obs_dates) {
+  temp_obs <- arrow::read_feather(obs_feather) %>%
+    filter(site_id %in% modeled_sites) %>%
+    filter(date >= start_date & date <= end_date) %>%
+    rename(time = date, temperature = temp) %>% # For consistent naming throughout pipeline
+    select(-source)
   
+  eval_sites <- temp_obs %>%
+    group_by(site_id) %>%
+    summarize(n_dates = length(unique(time))) %>% filter(n_dates >= min_obs_dates) %>%
+    pull(site_id)
+  
+  filtered_temp_obs <- filter(temp_obs, site_id %in% eval_sites)
+  
+  return(filtered_temp_obs)
 }
 
-# bias fxn
-calc_bias <- function(pred_obs, grouping_var) {
-  pred_obs %>%
-    group_by(!!grouping_var) %>%
+#' @title Match predictions to observations
+#' @description Interpolate the model predictions to the depths of the observations
+#' @param eval_obs temperature observations to use in evaluation, grouped by site.
+#' The function maps over these groups.
+#' @param eval_preds temperature predictions to use in evaluation, grouped by site.
+#' The function maps over these groups.
+#' @return A tibble with predictions matched to observations (on available dates)
+#' by date and by depth
+match_pred_obs <- function(eval_obs, eval_preds) {
+  # Filter model predictions to those on dates with obercations
+  eval_preds <- eval_preds %>%
+    mutate(depth = as.numeric(depth)) %>% 
+    filter(time %in% eval_obs$time) %>%
+    rename(pred = temperature)
+  
+  eval_obs <- eval_obs %>%
+    rename(obs = temperature)
+  
+  # match up preds to test_obs, interpolating predictions to match the observation depths
+  pred_obs <- bind_rows(lapply(unique(eval_obs$time), function(selected_time) {
+    pred_1d <- filter(eval_preds, time == selected_time, !is.na(depth))
+    obs_1d <- filter(eval_obs, time == selected_time)
+    interp_1d <- tryCatch({
+      if(nrow(pred_1d) == 0) stop(sprintf('no predictions on %s', selected_time))
+      if(min(pred_1d$depth) != 0) warning(sprintf('no prediction at 0m on %s', selected_time))
+      mutate(obs_1d, pred = approx(x=pred_1d$depth, y=pred_1d$pred, xout=obs_1d$depth, rule=1)$y)
+    }, error=function(e) {
+      message(sprintf('approx failed on %s: %s', selected_time, e$message))
+      mutate(obs_1d, pred = NA)
+    })
+    return(interp_1d)
+  }))
+  
+  return(pred_obs)
+}
+
+#' @title prep pred-obs data for evaluation
+#' @description filter pred-obs data to selected_depth and add grouping
+#' variables for evaluation.
+#' @param pred_obs the tibble of matched observations and predictions
+#' @param selected_depth depth to which to filter the pred-obs data
+#' @return a tibble of matched predictions and observations at the
+#' selected depth with the difference between the prediction and observation
+#' (pred_diff), the year, doy, month, and season of each pred-obs pair,
+#' and the temperature bin into which each observation falls 
+prep_data_for_eval <- function(pred_obs, selected_depth) {
+  eval_pred_obs <- pred_obs %>%
+    filter(depth==selected_depth) %>%
+    mutate(pred_diff = pred - obs,
+           year = year(time),
+           doy = yday(time),
+           month = month(time),
+           season = case_when(
+             month >= 3 & month <= 5 ~ 'spring',
+             month >= 6 & month <= 8 ~ 'summer',
+             month >= 9 & month <= 11 ~ 'fall',
+             TRUE ~ 'winter'
+           ),
+           temp_bin = 2*ceiling(obs/2))
+  
+  return(eval_pred_obs)
+}
+
+#' @title Calcuate bias
+#' @description Calculate the bias of model predictions over a 
+#' specified `grouping_var`
+#' @param eval_pred_obs a tibble of the matched model predictions and 
+#' observations, along with grouping variables for evaluation
+#' @param grouping_var the variable by which to group `pred-obs`
+#' before calculating the bias
+#' @return
+calc_bias <- function(eval_pred_obs, grouping_var) {
+  eval_pred_obs %>%
+    group_by(!!sym(grouping_var)) %>%
     summarize(bias = median(pred_diff))
 }
 
-# RMSE fxn
-calc_rmse <- function(pred_obs, grouping_var) {
-  pred_obs %>%
-    group_by(!!grouping_var) %>%
+#' @title Calculate rmse
+#' @description Calculate the rmse of model predictions over a 
+#' specified `grouping_var`
+#' @param eval_pred_obs a tibble of the matched model predictions and 
+#' observations, along with grouping variables for evaluation
+#' @param grouping_var the variable by which to group `pred-obs`
+#' before calculating the rmse
+#' @return a tibble grouped by the grouping_var, with a column
+#' for rmse
+calc_rmse <- function(eval_pred_obs, grouping_var) {
+  eval_pred_obs %>%
+    group_by(!!sym(grouping_var)) %>%
     summarize(rmse = sqrt(mean((pred_diff)^2, na.rm=TRUE)))
 }
 
-# plot fxn
-plot_barplot <- function(plot_df, y_var, x_var, depth, outfile) {
+#' @title Plot evaluation metrics as a bar plot
+#' @description generate a barplot of an evaluation metric, using the
+#' specified x and y variables
+#' @param plot_df a tibble of the matched model predictions and observations,
+#' along with grouping variables for evaluation
+#' @param driver the name of the driver used to generate the
+#' model predictions
+#' @param y_var the variable for the y-axis of the plot
+#' @param x_var the variable for the x-axis of the plot
+#' @param depth depth of the plotted pred-obs
+#' @param outfile The filepath for the exported png
+#' @return The filepath of the exported png 
+plot_evaluation_barplot <- function(plot_df, driver, y_var, x_var, depth, outfile) {
+  bar_plot <- plot_df %>%
+    ggplot(aes(x = get(x_var), y = get(y_var))) +
+    geom_col(fill='cadetblue3', color='cadetblue4') +
+    labs(title= sprintf("%s predictions: %s by %s", driver, y_var, x_var), 
+         x=sprintf("%s", x_var), 
+         y=sprintf("%s (\u00b0C)", y_var)) +
+    theme_bw()
   
   ggsave(filename=outfile, plot=bar_plot, dpi=300, width=10, height=6)
   return(outfile)
