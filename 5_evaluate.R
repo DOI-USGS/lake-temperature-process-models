c90be03237aec4dd3b595881fae9ddef9954e67c
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-06-03 13:34:16 -0500

    Fix comment typo

--- 5_evaluate.R
+++ 5_evaluate.R
@@ -9,7 +9,7 @@ p5 <- list(
   ###### Prep predictions and observations ######
   
   # Get vector of site_ids for which we have NLDAS output
-  # For now, evaluation only predictions for lakes within CASC states
+  # For now, evaluate only predictions for lakes within CASC states
   tar_target(p5_CASC_states,
              c('ND','SD','IA','MI','IN','IL','WI','MN','MO','AR','OH')),
   tar_target(p5_nldas_export_site_ids,
